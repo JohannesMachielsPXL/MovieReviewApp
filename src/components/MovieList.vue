c725bd165154592deca9fef10aab7d4367497c74
Johannes Machiels <johannes.machiels@student.pxl.be>
2023-04-28 15:01:29 +0200

    Initial commit
    Basic framework for the movie review app

---
+++ src/components/MovieList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">Welcome</h1>
+    <h3>
+      Select a movie to get started
+    </h3>
+    <div id="MovieList">
+      <table >
+        <tbody>
+        <tr>
+<!--          insert rating-->
+          <td class="movieRatingForTable">x x x x x</td>
+<!--          insert title-->
+          <td class="movieTitleForTable"> Movie 1 </td>
+        </tr>
+        <tr>
+          <td  class="movieRatingForTable">x x x</td>
+          <td  class="movieTitleForTable">Movie 2</td>
+        </tr>
+        <tr>
+          <td  class="movieRatingForTable">x x x x </td>
+          <td  class="movieTitleForTable">Movie 3</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+.movieRatingForTable{
+  text-align: right;
+}
+.movieTitleForTable{
+  text-align: left;
+  color: var(--color-heading);
+}
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+td {
+  text-align: left;
+}
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/MovieReview.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MovieItem from './MovieItem.vue'
+import MovieDetailsIcon from './icons/IconMovieDetails.vue'
+import MovieRatingIcon from './icons/IconMovieRating.vue'
+import MovieReviewsIcon from './icons/IconMovieReviews.vue'
+</script>
+
+<template>
+  <MovieItem>
+    <template #icon>
+      <MovieDetailsIcon />
+    </template>
+    <template #movieTitle>Insert Titel </template>
+    <template #movieRating>Insert movie rating stars</template>
+    <template #movieReviews>Insert movie Reviews List</template>
+  </MovieItem>
+
+</template>
+
+<style scoped>
+
+</style>
