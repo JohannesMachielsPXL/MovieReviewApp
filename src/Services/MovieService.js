c6b9ecef82aeb152808b85a9d7b6ff7859520ad5
KevinTheysPXL <kevin.theys@student.pxl.be>
2023-05-31 23:21:23 +0200

    selecteren van een Movie werkt
    werkt enkel in MovieListView zelf maar hier kunnen we denk ik wel mee verder

--- src/Services/MovieService.js
+++ src/Services/MovieService.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const MOVIE_API_BASE_URL = 'http://localhost:3000/movies'
+const MOVIE_API_BASE_URL = 'http://localhost:3000/movies/'
 
 class MovieService{
     getMovies(){
         return axios.get(MOVIE_API_BASE_URL);
+    }
+
+    getMovieNameById(id) {
+        return axios.get(MOVIE_API_BASE_URL + id);
     }
 }
 
--- src/components/MovieListView.vue
+++ src/components/MovieListView.vue
@@ -4,11 +4,12 @@
     <h3>
       Select a movie to get started
     </h3>
+    <h3>gekozen film: {{selectedMovie.name}}</h3>
     <div id="MovieList">
       <table>
         <tbody>
         <tr v-for = "movie in movies" v-bind:key = movie.id>
-          <td>{{movie.name}}</td>
+          <td><button @click="fetchMovie(movie.id)">{{movie.name}}</button></td>
         </tr>
         </tbody>
       </table>
@@ -22,6 +23,16 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
+}
+
+button  {
+  background: none;
+  border: none;
+}
+
+button:hover {
+  background: #00bd7e;
+  border-radius: 5px;
 }
 
 h3 {
@@ -46,17 +57,23 @@ td {
 
 <script>
 import MovieService from "@/services/MovieService";
-
 export default {
   name: 'Movies',
   data(){
-    return{ movies : []
+    return{
+      movies : [],
+      selectedMovie: ''
     }
   },
   methods: {
     getMovies(){
       MovieService.getMovies().then((response) => {
         this.movies = response.data;
+      });
+    },
+    fetchMovie(id) {
+      MovieService.getMovieNameById(id).then((response) => {
+        this.selectedMovie = response.data;
       });
     }
   },
